0f738bd1f5cc7e05cc449a878497e36770449e5d
pch6630 <pch6640@gmail.com>
2026-05-01 01:05:14 +0900

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -1,10 +1,7 @@
-//
-// Created by Perfare on 2020/7/4.
-//
-
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.game.packagename"
+// 실제 게임 패키지명으로 수정했습니다.
+#define GamePackageName "com.yuanl3.zugpshu"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
